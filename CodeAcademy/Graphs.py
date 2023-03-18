d19de782ac580a73ef0b951b8031251d10dc5f56
Alexis Tamayo <lexx426@gmail.com>
2023-03-17 22:47:02 -0500

    Addition of knowledge checks for BFS and DFS in Python

--- CodeAcademy/Graphs.py
+++ CodeAcademy/Graphs.py
@@ -92,3 +92,91 @@ some_important_graph = {
 # In a DFS implementation looking for a path between two points, what happens if the current vertex has the target value?
 if current_vertex is target_value:
     return visited
+
+
+# Complete the if statement in this depth-first search implementation.
+
+def dfs(graph, current_vertex, target_value, visited = None):
+    if #What goes here? <---- :
+    visited = []
+    visited.append(current_vertex)
+    if current_vertex is target_value:
+        return visited
+    
+    for neighbor in graph[current_vertex]:
+        if neighbor not in visited:
+        return dfs(graph, neighbor, target_value, visited)
+      
+some_important_graph = {
+    'lava': set(['sharks', 'piranhas']),
+    'sharks': set(['lava', 'bees', 'lasers']),
+    'piranhas': set(['lava', 'crocodiles']),
+    'bees': set(['sharks']),
+    'lasers': set(['sharks', 'crocodiles']),
+    'crocodiles': set(['piranhas', 'lasers'])
+}
+
+# visited is None
+# If visited is None, then this is not a recursive call of the function and visited needs to be given the value of an empty list.
+
+
+# What’s the purpose of having a set called visited in a BFS Python implementation?
+# To store up all visited vertices so as not to revist any
+
+# Quiz: Graph Search: Python
+# Which for loop might be part of a breadth-first search Python implementation?
+
+# A:
+for neighbor in graph[current_vertex]:
+    if neighbor not in visited:
+        if neighbor is target_value:
+            return path + [neighbor]
+        else:
+        search_queue.append([neighbor, path + [neighbor]])
+        
+    # B:
+for neighbor in graph[current_vertex]:
+    if neighbor not in visited:
+        return search(graph, neighbor, target_value, visited)
+    
+# This would be a DFS for loop which includes a recursive return statement for unvisited vertices. BFS implemetations employ a search queue to find unvisited vertices
+
+# Answer is A
+
+
+# Quiz: Graph Search: Python
+# Complete the return statement for the base case of this DFS implementation:
+
+def dfs(graph, current_vertex, target_value, visited = None):
+    if visited is None:
+        visited = []
+    visited.append(current_vertex)
+    if current_vertex is target_value:
+        return # What goes here?
+    
+    for neighbor in graph[current_vertex]:
+        if neighbor not in visited:
+            return dfs(graph, neighbor, target_value, visited)
+# visited
+        
+
+# Why is visited included in the parameters of this depth-first search implementation?
+
+def dfs(graph, current_vertex, target_value, visited = None):
+    if visited is None:
+        visited = []
+        
+    visited.append(current_vertex)
+    
+    if current_vertex is target_value:
+        return visited
+    
+    for neighbor in graph[current_vertex]:
+        if neighbor not in visited:
+            return dfs(graph, neighbor, target_value, visited)
+        
+# visited will be passed in to continue building out the path during recursive calls
+
+# Inside a breadth-first search implementation, you have a for loop of the current vertex’s neighboring vertices. What needs to be true in this loop in order for the path to be returned?
+
+# the neighboring vertex has been visited AND the neighboring vertex has the target value
