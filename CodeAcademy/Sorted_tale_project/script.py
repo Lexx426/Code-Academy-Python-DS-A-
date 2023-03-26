1ae185be9daa20c0616ab6fd424d55bad5d2be87
Alexis Tamayo <lexx426@gmail.com>
2023-03-25 19:29:41 -0500

    Completion of Book shelf project where I made and used quicksort and bubble sort algorithms

--- CodeAcademy/Sorted_tale_project/script.py
+++ CodeAcademy/Sorted_tale_project/script.py
@@ -2,6 +2,8 @@ import utils
 import sorts
 
 bookshelf = utils.load_books('books_small.csv')
+long_bookshelf = utils.load_books('books_large.csv')
+
 for book in bookshelf:
     print(book['title'])
 
@@ -10,4 +12,29 @@ print(ord(" "))
 print(ord("A"))
 # when sorting, we do not want to take account the case of the letters. For example, “cats” should come before “Dogs”, even though ord("c") > ord("D") is True.
 
-book['author_lower'] = book['author'].lower()
+def by_title_ascending( book_a, book_b):
+    return book_a['title_lower'] > book_b ['title_lower']
+
+sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
+
+for book in sort_1:
+  print(book['title'])
+
+def by_author_ascending( book_a, book_b):
+  return book_a['author_lower'] > book_b['author_lower']
+
+bookshelf_v1 = bookshelf.copy()
+
+sort_2 = sorts.bubble_sort(bookshelf_v1, by_author_ascending)
+
+for book in sort_2:
+    print(book['author'])
+
+    bookshelf_v2 = bookshelf.copy()
+
+sorts.quicksort(bookshelf_v2, 0, len(bookshelf_v2) - 1, by_author_ascending)
+
+def by_total_length(book_a, book_b):
+    return(len(book_a['author_lower']) + len(book_a['title_lower']) > len(book_b['author_lower']) + len (book_b['title_lower']))
+
+sorts.bubble_sort( long_shelf, by_total_length)
